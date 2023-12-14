19d268074df519c64f6357a8f95ae497bd72211f
Ariston <aristoncatipay123@gmail.com>
2023-12-14 22:19:40 +0800

    Create the home page and the view function that pulls the data.

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, Request, Form, status
+
+from starlette.responses import RedirectResponse
+from starlette.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
 
 @app.get("/")
-def read():
-    return {"Hello": "World"}
+def read(request: Request, db: Session = Depends(get_db)):
+    todo = db.query(models.Todo).all()
+    return templates.TemplateResponse("base.html",
+                                      {"request": request, "todo_list": todo})
