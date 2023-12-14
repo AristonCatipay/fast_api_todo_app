9044016febc363b8de08977492aac41cb7e53748
Ariston <aristoncatipay123@gmail.com>
2023-12-14 22:32:39 +0800

    Create the read functionality.

--- app.py
+++ app.py
@@ -3,9 +3,23 @@ from fastapi import FastAPI, Depends, Request, Form, status
 from starlette.responses import RedirectResponse
 from starlette.templating import Jinja2Templates
 
+from sqlalchemy.orm import Session
+import models
+from database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
 templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 def read(request: Request, db: Session = Depends(get_db)):
